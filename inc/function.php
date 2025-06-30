c6355784d96e5110539c457ce0f031ffb9c991c1
Nomena-coder <raobelinanomenjanahary@gmail.com>
2025-06-30 15:42:18 +0300

    sync

--- inc/function.php
+++ inc/function.php
@@ -25,10 +25,11 @@
         $sql = "SELECT * FROM dept_emp 
         JOIN employees 
         ON dept_emp.emp_no = employees.emp_no
-        WHERE dept_emp.dept_no = '%s'";
+        WHERE dept_emp.dept_no = '%s' AND dept_emp.to_date = '9999-01-01'
+        ORDER BY employees.first_name";
         $sql = sprintf($sql, $id_dept);
         $data = [];
-        $resultat = mysqli_query();
+        $resultat = mysqli_query(dbconnect(), $sql);
         while($data[] = mysqli_fetch_assoc($resultat));
         unset($data[count($data)-1]);
         return $data;
--- pages/fiche.php
+++ pages/fiche.php
@@ -1,5 +1,17 @@
 <?php
 
-    $emp = 
+    $emp = getEmployes($_GET['dept_no']);
 
 ?>
+
+<div class="container-fluid">
+    <ul>
+        <?php foreach($emp as $value) { ?>
+            <li>
+                <a href="modal.php?page=fiche_emp&emp_no=<?php echo $value['emp_no']; ?>">
+                    <?php echo $value['first_name']." ".$value['last_name']; ?>
+                </a>
+            </li>
+        <?php } ?>
+    </ul>
+</div>
