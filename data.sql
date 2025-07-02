abdf95d1132552402c72baf16f50bfd7c67f1816
Indra-Ramp <ramparany.indra@gmail.com>
2025-07-02 14:48:55 +0300

    data

--- data.sql
+++ data.sql
@@ -3,7 +3,9 @@ employees.first_name, employees.last_name, employees.gender, employees.birth_dat
 FROM dept_manager JOIN departments ON dept_manager.dept_no = departments.dept_no
 JOIN employees ON dept_manager.emp_no = employees.emp_no;
 
-CREATE OR REPLACE VIEW v_emp_lib AS SELECT employees.*, departments.dept_name, departments.dept_no, dept_emp.from_date, dept_emp.to_date, titles.title 
+CREATE OR REPLACE VIEW v_emp_lib AS SELECT employees.*, departments.dept_name, departments.dept_no, dept_emp.from_date, dept_emp.to_date 
 FROM employees 
 JOIN dept_emp ON dept_emp.emp_no = employees.emp_no
 JOIN departments ON dept_emp.dept_no = departments.dept_no;
+
+CREATE OR REPLACE VIEW v_salary_emploi as select s.salary, t.title, e.* from salaries s join employees e on s.emp_no = e.emp_no join titles t on e.emp_no=t.emp_no;
